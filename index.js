e89bea093ec1399a18782cbd735222f2219ae78b
Miguel_Mendez <ing.mendez999@gmail.com>
2022-05-31 15:00:09 -0500

    control de spa

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -31,6 +32,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/uploads', require('./routes/uploads'));
+
+//lo ultimo
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html'))
+});
 
 //Inico
 app.listen( process.env.PORT, () => {
